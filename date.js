5123e81aac7581258152d01f89592629497f22d3
MaX Falstein <MaXFalstein@users.noreply.github.com>
2018-05-24 13:40:22 +0100

    Update date.js

--- date.js
+++ date.js
@@ -1,6 +1,6 @@
 var d = new Date();
 var year = d.getFullYear().toString();
-var m = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+var m = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var month = m[d.getMonth()];
 var space = " ";
 var date = month.concat(space.concat(year));
